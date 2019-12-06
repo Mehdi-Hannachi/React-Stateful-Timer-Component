c0491ed7517a6952e77567e06fb57ac7ceac6acf
Mehdi <hannachimehdi69@gmail.com>
2019-12-06 11:33:43 +0100

    Final version

--- stateful-timer-component/src/App.js
+++ stateful-timer-component/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./Components/Time.css";
+import Timer from "./Components/Time";
+import Icon from "./Components/icon.JPG"
+import Ic from "./Components/ic.JPG"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="telephone">
+<img src={Ic} className="ic"  alt="icon"/>
+
+<Timer/>
+<img src={Icon} className="icon" alt="ic"/>
+
     </div>
+  
+   
   );
 }
-
 export default App;
